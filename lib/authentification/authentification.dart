357c821540d6c9b8454dfa5c43a1b4980a4b0eb8
Tergel <dembeefr@gmail.com>
2020-09-03 19:21:58 +0200

    Drawer

--- lib/authentification/authentification.dart
+++ lib/authentification/authentification.dart
@@ -14,6 +14,61 @@ class _AuthentificationState extends State<Authentification> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            children: <Widget>[
+              DrawerHeader(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        children: [Icon(Icons.person_pin), Text("Tergel t")],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(""),
+                          FlatButton(
+                              onPressed: () {},
+                              child: Text("Modifier le profil"))
+                        ],
+                      )
+                    ],
+                  ),
+                  decoration:
+                      BoxDecoration(color: Theme.of(context).primaryColor)),
+              ListTile(
+                title: Text('Rechercher un hôtel'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: Text('Mes réservations'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: Text('Mon compte'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: Text('Paramètres'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
           bottom: TabBar(
             tabs: <Widget>[
@@ -27,10 +82,7 @@ class _AuthentificationState extends State<Authentification> {
           title: Text("Authentification"),
         ),
         body: TabBarView(
-          children: [
-            Connexion(),
-            Inscription()
-          ],
+          children: [Connexion(), Inscription()],
         ),
       ),
     );
